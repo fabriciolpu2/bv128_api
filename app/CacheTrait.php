65f53119db08d26337f068e4616c12e8ce5e201a
pablo <pablomiranda304@gmail.com>
2026-03-26 09:55:11 -0400

    descritores

--- app/CacheTrait.php
+++ app/CacheTrait.php
@@ -7,9 +7,12 @@ use Predis\Connection\ConnectionException;
 
 trait CacheTrait
 {
-    public function cache($key, $callback, $expire = null)
+    public function cache($key, $callback, $expire = null, ?bool $useCache = false)
     {
         try {
+            if (!$useCache) {
+                return $callback;
+            }
             return Cache::remember($key, $expire ?? config('cache.ttl'), $callback);
         } catch (ConnectionException $exception) {
             return $callback();
